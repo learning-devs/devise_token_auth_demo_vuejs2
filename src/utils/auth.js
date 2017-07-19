e5c3d7ad56f377a618fc50b3af5c204a072bcd5d
MIGUEL ANGEL ARENAS CORREA <marenaa@contratista.epm.co>
2017-07-19 12:24:09 -0500

    Cambios de nombre archivo localStorageVariable

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,6 @@
 import { endpoints } from './../models/endpoints.js'
-import { header_names } from './../models/storageLocalVariables.js'
-import { user_names } from './../models/storageLocalVariables.js'
+import { header_names } from './../models/localStorageVariables.js'
+import { user_names } from './../models/localStorageVariables.js'
 
 
 export default{
