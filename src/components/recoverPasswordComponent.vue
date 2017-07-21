1d23a818f5b4a6691632e17bdbda5454c9534336
maac4422 <miguelarenas98@hotmail.com>
2017-07-21 15:22:15 -0500

    merge

--- src/components/recoverPasswordComponent.vue
+++ src/components/recoverPasswordComponent.vue
@@ -24,26 +24,6 @@
 				</md-card-content>
 			</md-card>
 		</div>
-		<md-card md-with-hover>
-
-			<md-card-header>
-				<div class="md-display-1">Recuperar Contraseña</div>
-			</md-card-header>
-
-			<md-card-content>
-				<form v-on:submit.prevent="recover">
-
-					<md-input-container>
-						<md-icon>account_circle</md-icon>
-						<label>Email</label>
-						<md-input type="email" v-model="user.email"></md-input>
-					</md-input-container>
-
-					<md-button class="md-raised md-accent" md-theme="blue">Recuperar Contraseña</md-button>
-
-				</form>
-			</md-card-content>
-		</md-card>
 	</div>
 </template>
 
